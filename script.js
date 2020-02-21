fa4ecacccbf52c1fe1ebd35400ade575ab4b5ade
ejiro <ejiro1994@gmail.com>
2020-02-21 20:11:27 +0000

    menu items and titile

--- script.js
+++ script.js
@@ -7,3 +7,43 @@ const options = {
 };
 const swup = new Swup(options);
 AOS.init();
+
+
+anime.timeline({loop: false}).add({
+    targets: '.ml5 .line',
+    opacity: [0.5,1],
+    scaleX: [0, 1],
+    easing: "easeInOutExpo",
+    duration: 700,
+    delay: 1000
+  }).add({
+    targets: '.ml5 .line',
+    duration: 600,
+    easing: "easeOutExpo",
+    translateY: (el, i) => (-0.625 + 0.625*2*i) + "em"
+  }).add({
+    targets: '.ml5 .letters-left',
+    opacity: [0,1],
+    translateX: ["0.5em", 0],
+    easing: "easeOutExpo",
+    delay: 500,
+    duration: 1000,
+    offset: '-=600'
+  }).add({
+    targets: '.ml5 .letters-right',
+    opacity: [0,1],
+    translateX: ["-0.5em", 0],
+    easing: "easeOutExpo",
+    delay: 50,
+
+    duration: 1000,
+    offset: '-=600'
+  }).add({
+    targets: '.ml5',
+    opacity: 1,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+  
